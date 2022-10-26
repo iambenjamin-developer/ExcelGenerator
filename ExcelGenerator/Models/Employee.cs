55452aab191a266147ab5ae744a8cd6e838d8877
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-10-26 15:07:51 -0300

    models

---
+++ ExcelGenerator/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace ExcelGenerator.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ ExcelGenerator/Models/ImportExcelDTO.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ExcelGenerator.Models
+{
+    public class ImportExcelDTO
+    {
+        public IFormFile File { get; set; }
+    }
+}
